13958d4ad6dc5df9679bccbb18af0f410485b3a7
Squik <69891223+Wildwolf210@users.noreply.github.com>
2020-11-16 10:11:04 +0530

    v1.0.9
    Minor bugs fixed
    - Spaces are automatically replaced by '%20'.

--- client/search.js
+++ client/search.js
@@ -6,22 +6,27 @@ class Search {
     this.options = options;
   }
   google(query) {
-    if (query.includes(' ')) { query.replace(/ /gi, '%20')}
-    var searchQuery = "https://www.google.com/search?q=" + query;
+    var querySearch;
+    if (query.includes(' ')) { querySearch = query.replace(/ /gi, '%20')};
+    var searchQuery = "https://www.google.com/search?q=" + querysearch;
     return searchQuery;
   }
   youtube(query) {
-    var searchQuery = "https://www.youtube.com/search?q=" + query;
+    var querySearch;
+    if (query.includes(' ')) { querySearch = query.replace(/ /gi, '%20')};
+    var searchQuery = "https://www.youtube.com/search?q=" + querySearch;
     return searchQuery;
   }
   googleMaps(query) {
-    var searchQuery = "https://www.google.com/maps?q=" + query;
+    var querySearch;
+    if (query.includes(' ')) { querySearch = query.replace(/ /gi, '%20')};
+    var searchQuery = "https://www.google.com/maps?q=" + querySearch;
     return searchQuery;
   }
   googleTranslate(query) {
     let from = query.from ? query.from: 'auto';
     let to = query.to ? query.to: 'auto';
-    let text = query.text ? query.text : ' ';
+    let textInitialise = query.text ? query.text : ' ';
     let checkFrom = languages[from];
     let checkTo = languages[to];
     if (from != 'auto' && checkFrom === undefined) { 
@@ -30,11 +35,14 @@ class Search {
     if (to != 'auto' && checkTo === undefined) { 
       errors.searchError('Invalid language code.')
     }
+    let text = '';
+    if(textInitialise.includes(' ')) { text = textInitialise.replace(/ /gi, '%20')}
     var searchQuery = `https://translate.google.com/?q=#view=home&op=translate&sl=${from}&tl=${to}&text=${text}`
     return searchQuery;
   }
-  googlePlayStore(query, categorySearch) {
-    if (query.includes(' ')) { query.replace(/ /gi, '%20')}
+  googlePlayStore(querySearch, categorySearch) {
+    let query = '';
+    if (querySearch.includes(' ')) { query.replace(/ /gi, '%20')}
     let category = categorySearch ? `&c=${categorySearch}` : ' ';
     let q = query ? `${query}` : ' ';
     const categoryList = [
